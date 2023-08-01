57bbe24aa5d7aba91e2c918e1919d693d5dc7d18
TorgeirBrenn <torgeir.brenn@gmail.com>
2023-08-01 10:55:57 +0200

    completes v1.0

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,13 +1,17 @@
 import asyncio
 import os
+import time
 
 import dotenv
 from process import (
     add_batch_to_df,
+    batch_stream,
     convert_batch_to_dataframe,
+    convert_to_dataframe,
     empty_batch,
     rank_by_speed,
-    validate_and_batch_stream,
+    strip_data,
+    validate_stream,
 )
 from rich.live import Live
 from streaming import get_bw_stream, get_bw_token
@@ -15,18 +19,100 @@ from visualize import dataframe_to_table
 
 dotenv.load_dotenv()
 
+DEBUG_MODE = False
 GET_NEW_TOKEN = True
+UPDATE_INTERVAL_SECONDS = 1
+NUM_VESSELS_DISPLAYED = 10
+
+# Keep all data from the last X seconds or the most recent Y entries, whichever is more inclusive
+DATA_TIME_TO_LIVE_SECONDS = 10
+NUM_MESSAGES_TO_KEEP = 1000
 
 
-async def main():
-    print("Initializing FleetSpeed! Remember to always put safety first at sea.")
-    token = get_bw_token() if GET_NEW_TOKEN else os.environ["TOKEN"]
+def initialize_with_token(func):
+    """
+    A decorator that initializes a FleetSpeed client with a token.
 
+    Before the execution of the decorated function, it prints an initialization message and
+    fetches a Bearer token. The token is either newly obtained by calling `get_bw_token` or
+    fetched from the environment variable "TOKEN".
+
+    This decorator allows for reusability of token fetching and the initialization message for
+    multiple functions that act as clients under different circumstances.
+
+    Args:
+        func (function): The function being decorated. The function must be structurally compatible
+        for receiving a token as the first argument.
+
+    Returns:
+        function: The decorated function, with the token as the first argument.
+    """
+
+    def wrapper_func(*args, **kwargs):
+        print("Initializing FleetSpeed! Remember to always put safety first at sea.")
+        token = get_bw_token() if GET_NEW_TOKEN else os.environ["TOKEN"]
+        return func(token, *args, **kwargs)
+
+    return wrapper_func
+
+
+@initialize_with_token
+async def main_single_batch(token: str):
+    """
+    Asynchronously streams AIS messages, processes and ranks data for vessel speeds.
+
+    This function initializes with an empty data frame. It then continuously streams and
+    validates the AIS messages by using the provided token.
+
+    Every `UPDATE_INTERVAL_SECONDS`, it processes the received AIS messages into a dataframe,
+    ranks them by speed, and updates a live table with the top vessels.
+
+    Finally, it uses the `strip_data` function to manage the size of the stored messages.
+
+    Args:
+        token (str): Token to authenticate the AIS stream source.
+    """
+    full_df = empty_batch()
+
+    data = []
+    start_time = time.time()
+
+    with Live(dataframe_to_table(full_df), refresh_per_second=5) as live:
+        async for ais_msg in validate_stream(get_bw_stream(token)):
+            data.append(ais_msg)
+
+            if time.time() - start_time > UPDATE_INTERVAL_SECONDS:
+                # Process the data. Usually takes less than 1/100th of a second.
+                timer = time.time()  # debug purposes
+
+                df = convert_to_dataframe(data)
+                top = rank_by_speed(df).head(NUM_VESSELS_DISPLAYED)
+                table = dataframe_to_table(top)
+                live.update(table)
+
+                # Reset
+                start_time = time.time()
+                data = strip_data(
+                    data,
+                    time_to_live_secs=DATA_TIME_TO_LIVE_SECONDS,
+                    num_messages_to_keep=NUM_MESSAGES_TO_KEEP,
+                )
+
+                if DEBUG_MODE:
+                    print(
+                        f"Used {len(df)} messages and {time.time() - timer}s to display."
+                    )  # -> Used 1518 messages and 0.004235029220581055s to display.
+
+
+@initialize_with_token
+async def main_multi_batch(token: str):
+    """Alternative approach to main_single_batch(), found to be less effective and was not
+    sufficiently tuned."""
     batch_number = 0
     full_df = empty_batch()
 
     with Live(dataframe_to_table(full_df), refresh_per_second=5) as live:
-        async for batch in validate_and_batch_stream(get_bw_stream(token)):
+        async for batch in batch_stream(validate_stream(get_bw_stream(token))):
             batch_df = convert_batch_to_dataframe(batch, batch_number)
             batch_number += 1
 
@@ -38,4 +124,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main_single_batch())
--- pysrc/process.py
+++ pysrc/process.py
@@ -1,12 +1,25 @@
 from datetime import datetime
-from time import time
+from time import time, time_ns
 from typing import AsyncGenerator, TypedDict
 
 import polars as pl
 from pydantic import BaseModel, ValidationError, confloat, constr, field_validator
 
 
-class VesselData(BaseModel):
+class VesselDataModel(BaseModel):
+    """
+    A Pydantic model that represents vessel data.
+
+    This model describes the data structure of a vessel, including its MMSI number, its speed
+    over ground, and a timestamp. It also includes two methods for validating and casting the
+    'mmsi' and 'msgtime' fields.
+
+    Attributes:
+        mmsi (str): An nine-digit Maritime Mobile Service Identity number.
+        speedOverGround (float): The speed of the vessel over ground. Must be >= 0.0.
+        msgtime (datetime): A datetime object representing when the message was sent.
+    """
+
     mmsi: constr(min_length=9, max_length=9, pattern=r"^\d+$")
     speedOverGround: confloat(ge=0.0)
     msgtime: datetime
@@ -23,72 +36,201 @@ class VesselData(BaseModel):
 
 
 class VesselDataDict(TypedDict):
+    """
+    A TypedDict representing the vessel data and its creation time.
+
+    This TypedDict is similar to VesselDataModel, but includes an additional field - the ns
+    precision creation time of the object itself.
+
+    Attributes:
+        mmsi (str): An nine-digit Maritime Mobile Service Identity number.
+        speedOverGround (float): The speed of the vessel over ground.
+        msgtime (datetime): A datetime object representing when the message was sent.
+        created_at (int): The ns precision creation time of this object.
+    """
+
     mmsi: str
     speedOverGround: float
     msgtime: datetime
+    created_at: int  # ns
 
 
 def validate_extract_vessel_data(line: str) -> VesselDataDict | None:
     """
     Validates and extracts certain fields from a JSON string representing vessel data.
 
-    This function takes a JSON string as input, attempts to parse and validate it
-    according to the `VesselData` model. If the validation is successful,
-    it returns a dictionary containing 'mmsi' and 'speedOverGround'.
+    This function takes a JSON string as input, attempts to parse and validate it according to the
+    `VesselData` model. If the validation is successful, it returns a dictionary containing
+    'mmsi' and 'speedOverGround'.
 
-    If the validation fails due to a ValidationError (i.e., the input does not match
-    the `VesselData` model), the function will return None.
+    If the validation fails due to a ValidationError (i.e., the input does not match the
+    `VesselData` model), the function will return None.
 
     Args:
         line (str): A JSON string representing vessel data.
 
     Returns:
-        VesselDataDict | None: A dictionary with 'mmsi' and 'speedOverGround' fields
-        if the input string is valid; None otherwise.
+        VesselData | None: An object with 'mmsi', 'speedOverGround' and 'msgtime' attributes if the
+        input string is valid; None otherwise.
     """
     try:
-        vessel_data = VesselData.model_validate_json(line)
+        vessel_data_model = VesselDataModel.model_validate_json(line)
     except ValidationError:
         # Ignore data errors for now
         return
-    return vessel_data.model_dump()
+
+    vessel_dict = vessel_data_model.model_dump()
+    vessel_dict["created_at"] = time_ns()
+
+    return vessel_dict
 
 
-async def validate_and_batch_stream(
+async def validate_stream(
     line_generator: AsyncGenerator[str, None]
-) -> AsyncGenerator[list[VesselDataDict], None]:
-    """Validates each line of data and strips out fields which are not needed. Then returns mini-
-    batches corresponding to one second."""
-    next_batch_time = time() + 1
-    batch = []
+) -> AsyncGenerator[VesselDataDict, None]:
+    """
+    Asynchronously validate and extract vessel data from a text stream.
 
+    The function takes lines from the `line_generator` and validates the line content. If the
+    line contains valid vessel data, it is yielded in the VesselDataDict format.
+
+    Args:
+        line_generator (AsyncGenerator[str, None]): An asynchronous generator yielding lines of
+        text.
+
+    Yields:
+        VesselDataDict: A dictionary containing validated vessel data.
+    """
     async for line in line_generator:
         vessel_data = validate_extract_vessel_data(line)
         if vessel_data:
-            batch.append(vessel_data)
+            yield vessel_data
+
+
+async def batch_stream(
+    ais_msg_generator: AsyncGenerator[VesselDataDict, None],
+    batch_interval_secs: int = 1,
+) -> AsyncGenerator[list[VesselDataDict], None]:
+    """
+    Asynchronously batch vessel data messages in the given time interval.
+
+    The function forms batches from the `ais_msg_generator`, each batch covering the provided
+    `batch_interval_secs` duration. Each batch is then yielded as a list of VesselDataDict.
+
+    Args:
+        ais_msg_generator (AsyncGenerator[VesselDataDict, None]): An asynchronous generator
+            yielding vessel data messages represented as dictionaries.
+        batch_interval_secs (int, optional): The time interval for each batch in seconds.
+
+    Yields:
+        list[VesselDataDict]: A list containing batches of vessel data messages.
+    """
+    next_batch_time = time() + batch_interval_secs
+    batch = []
+
+    async for ais_msg_generator in ais_msg_generator:
+        batch.append(ais_msg_generator)
 
         if (curr_time := time()) > next_batch_time:
             yield batch
             batch = []
-            next_batch_time = curr_time + 1
+            next_batch_time = curr_time + batch_interval_secs
+
+
+def strip_data(
+    data: list[VesselDataDict], time_to_live_secs: int, num_messages_to_keep: int
+) -> list[VesselDataDict]:
+    """
+    Filters and returns the most recent data based on the Time To Live and the number of
+    messages to keep.
+
+    Data is retained if it is either withing the Time To Live or the most recent messages, or both.
+
+    Args:
+        data (list[VesselDataDict]): A list of vessel data.
+        time_to_live_secs (int): The duration in seconds for which messages are considered recent.
+        num_messages_to_keep (int): The minimum number of messages to keep.
+
+    Returns:
+        list[VesselDataDict]: A list of data that matched the filtering rules mentioned above.
+    """
+    # Assumes ordered data!
+    now = time_ns()
+    time_cutoff = now - time_to_live_secs * 1e9
+
+    # Case 1: Fewer messages than what we want to keep => keep everything
+    if len(data) <= num_messages_to_keep:
+        return data
+
+    # Case 2: The time cutoff would result in fewer than num_messages_to_keep => return fixed number
+    # of messages
+    if data[-num_messages_to_keep]["created_at"] < time_cutoff:
+        return data[-num_messages_to_keep:]
+
+    # Case 3: Return data more recent than the cutoff
+    for n, entry in enumerate(data):
+        if entry["created_at"] >= time_cutoff:
+            return data[n:]
 
 
 def convert_batch_to_dataframe(
     data: list[VesselDataDict], batch_number: int
 ) -> pl.DataFrame:
-    df = pl.from_dicts(data, schema=VesselDataDict.__annotations__)
+    """
+    Converts a data batch into a dataframe and assigns a batch number to it.
+
+    Args:
+        data (list[VesselDataDict]): A list of vessel data dictionaries.
+        batch_number (int): The batch number to be assigned.
+
+    Returns:
+        DataFrame: Polars DataFrame created from the batch data.
+    """
+    df = convert_to_dataframe(data)
     df = df.with_columns(pl.lit(batch_number).alias("batch_number"))
 
     return df
 
 
+def convert_to_dataframe(data: list[VesselDataDict]) -> pl.DataFrame:
+    """
+    Converts a list of vessel data dictionaries into a dataframe.
+
+    Args:
+        data (list[VesselDataDict]): A list of vessel data dictionaries.
+
+    Returns:
+        DataFrame: Polars DataFrame created from the data.
+    """
+    df = pl.from_dicts(data, schema=VesselDataDict.__annotations__)
+
+    return df
+
+
 def empty_batch() -> pl.DataFrame:
+    """
+    Returns an empty dataframe structured as a batch of vessel data.
+
+    Returns:
+        DataFrame: an empty Polars DataFrame.
+    """
     return convert_batch_to_dataframe([], batch_number=0)
 
 
 def add_batch_to_df(
     batch: pl.DataFrame, full_df: pl.DataFrame, num_batches: int | None = None
 ) -> pl.DataFrame:
+    """
+    Adds a batch of data onto an existing dataframe, keeping the most recent batches.
+
+    Args:
+        batch (DataFrame): New batch of data.
+        full_df (DataFrame): Existing data.
+        num_batches (int, optional): Number of recent batches to keep in the full dataframe.
+
+    Returns:
+        DataFrame: The dataframe with new batch added, keeping the most recent batches.
+    """
     df = pl.concat([full_df, batch])
 
     if num_batches is not None:
@@ -102,6 +244,15 @@ def add_batch_to_df(
 
 
 def rank_by_speed(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Ranks the vessel data by maximum speed over ground.
+
+    Args:
+        df (DataFrame): Vessel data in DataFrame form.
+
+    Returns:
+        DataFrame: The input dataframe with vessels ranked by speed in descending order.
+    """
     ranked = (
         df.select(pl.col("speedOverGround").max().over("mmsi"), "mmsi", "msgtime")
         .unique()
